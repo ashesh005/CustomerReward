af138e8d7537313fce6764912f0be94a528811f4
ashesh005 <66697472+ashesh005@users.noreply.github.com>
2020-06-09 16:00:30 -0400

    App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import { CustomerRewards } from "./components/customerRewards";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CustomerRewards />
+    </div>
+  );
+}
+
